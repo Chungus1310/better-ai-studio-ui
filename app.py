0fe8e92d37b175fb84b809b5c94cbe2e217b413e
Chun <132124373+Chungus1310@users.noreply.github.com>
2025-02-20 04:25:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -188,4 +188,5 @@ def import_chat():
     return jsonify({"success": True})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
